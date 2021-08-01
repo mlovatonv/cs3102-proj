d896bf5f5cebe014b39887d7dfeb89e12b6f6716
Maria <mlovatonv@gmail.com>
2021-08-01 12:18:06 -0500

    Added initial files

---
+++ include/spatial/PMQuadTree.hpp
@@ -0,0 +1,10 @@
+#ifndef CS3102_PROJ_SPATIAL_PMQUADTREE_HPP_
+#define CS3102_PROJ_SPATIAL_PMQUADTREE_HPP_
+
+namespace spatial {
+
+class PMQuadTree {};
+
+}  // namespace spatial
+
+#endif  // CS3102_PROJ_SPATIAL_PMQUADTREE_HPP_
---
+++ include/spatial/PQ.hpp
@@ -0,0 +1,12 @@
+// https://www.postgresql.org/docs/current/libpq-example.html
+
+#ifndef CS3102_PROJ_SPATIAL_PQ_HPP_
+#define CS3102_PROJ_SPATIAL_PQ_HPP_
+
+namespace spatial {
+
+class PQ {};
+
+}  // namespace spatial
+
+#endif  // CS3102_PROJ_SPATIAL_PQ_HPP_
