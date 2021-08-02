df4d8eba33a14cb302afd9916ed51c69cfe9b6fe
Maria <mlovatonv@gmail.com>
2021-08-01 23:37:03 -0500

    Added unit tests

---
+++ test/test_PRQuadTree.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
+#include "catch2/catch.hpp"
+#include "spatial/PRQuadTree.hpp"
+
+using namespace spatial;
+
+TEST_CASE("search rectangle") {
+    PRQuadTree t(Rectangle(XY(0, 0), XY(10, 10)));
+
+    t.insert(XY(1, 1));
+    t.insert(XY(3, 3));
+    t.insert(XY(5, 5));
+
+    REQUIRE(t.search(Rectangle(XY(0, 0), XY(2, 2))) == std::vector<XY>{XY(1, 1)});
+    REQUIRE(t.search(Rectangle(XY(2, 2), XY(4, 4))) == std::vector<XY>{XY(3, 3)});
+    REQUIRE(t.search(Rectangle(XY(3, 3), XY(5, 5))) == std::vector<XY>{XY(3, 3), XY(5, 5)});
+}
